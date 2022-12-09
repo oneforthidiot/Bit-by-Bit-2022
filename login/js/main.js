5d209e714d4b4024b22e88f60a587958ea96dcc7
OneForthIdiot <harshitpandey64@gmail.com>
2022-12-09 21:00:21 +0530

    Adding Profie Page

--- login/js/main.js
+++ login/js/main.js
@@ -31,6 +31,6 @@ document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
-}
-}
+        }
+    }
 }
